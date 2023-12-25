06f9b89a95c9cf90beea619ca05e0f2cc54f5334
Ayanokoji Kiyotaka <kyilan.ntj@gmail.com>
2023-12-25 19:16:20 +0200

    done with nav

--- app.js
+++ app.js
@@ -6,3 +6,22 @@ window.addEventListener('scroll',()=> {
 document.querySelector('nav').classList.toggle
 ('window-scroll',window.scrollY > 0)
 })
+
+
+
+
+const main = document.querySelectorAll('.cardone');
+
+main.forEach(cardone => {
+    cardone.addEventListener('click', () => {
+cardone.classList.toggle('open');
+    });
+});
+
+
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navbarToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+});
